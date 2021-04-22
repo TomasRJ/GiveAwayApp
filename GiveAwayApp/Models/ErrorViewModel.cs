7a30955dc96deb51127b34ff7f6b02f856f4ba33
Tomas R J <tomasrj@outlook.dk>
2021-04-22 12:21:25 +0200

    Add project files.

---
+++ GiveAwayApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GiveAwayApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
