c8715777832c505f45e3323dbe63eacd3f077124
Tomas R J <tomasrj@outlook.dk>
2021-05-19 15:02:00 +0200

    Lavede Statistik MVC som viser spil sorteret efter valgt antal

---
+++ GiveAwayApp/Controllers/StatistikController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GiveAwayApp.Data;
+using GiveAwayApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GiveAwayApp.Controllers
+{
+    public class StatistikController : Controller
+    {
+        private readonly GiveAwayAppContext _context;
+        public StatistikController(GiveAwayAppContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var spilStatistikQuery = from spil in _context.Spil orderby spil.ValgtAntal descending select spil;
+
+            StatiskViewModel statiskVM = new StatiskViewModel
+            {
+                PopulæreSpilList = await spilStatistikQuery.ToListAsync()
+            };
+
+            return View(statiskVM);
+        }
+    }
+}
---
+++ GiveAwayApp/Models/StatiskViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiveAwayApp.Models
+{
+    public class StatiskViewModel
+    {
+        public List<Spil> PopulæreSpilList { get; set; }
+        public List<string> PopulæreGenreList { get; set; }
+        public int AntalBesøgere { get; set; }
+        public int AntalBrugere { get; set; }
+        public string[] LandeKodeList { get; set; }
+    }
+}
